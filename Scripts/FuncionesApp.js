a70a29e89b7b8b95ab187e25d2900fbbcaed3e72
Darwing Hernandez <70980484+Darwing99@users.noreply.github.com>
2021-04-19 18:08:01 -0600

    version 1

--- Scripts/FuncionesApp.js
+++ Scripts/FuncionesApp.js
@@ -11,7 +11,7 @@ $('#ver').click(function(){
 
 });
 $(document).ready(function() {
-  
+
     // SideNav Button Initialization
     $(".button-collapse").sideNav();
      // SideNav Scrollbar Initialization
@@ -19,7 +19,7 @@ $(document).ready(function() {
       var ps = new PerfectScrollbar(sideNavScrollbar);
   });
 
-  
+
 // Codigo de foto
 $("#subir").click(function(){
     document.getElementById("image").click();
@@ -56,17 +56,17 @@ $(document).ready(function(){
                 $('#form_empresa').css("opacity",".5");
             },
             success: function(response){
-          
+
                 $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Datos enviados satisfactoriamente.</span>');
-              
-            
+
+
          },
          error: function(error){
             $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Ocurrió un problema al momento de enviar.</span>');
          }
-              
-            
-        }); 
+
+
+        });
     });
 
 // INSERTAR ARTICULOS
@@ -89,7 +89,7 @@ $(document).ready(function(){
                 if(msg==0){
                     $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Datos enviados satisfactoriamente.</span>');
                 }else{
-                   
+
                     $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Ocurrió un problema al momento de enviar.</span>');
                 }
                 $('#idblog').css("opacity","");
@@ -113,21 +113,21 @@ $(document).ready(function(){
                 $('#guardar').attr("disabled","disabled");
                 $('#_agregar').css("opacity",".5");
             },
-          
+
             success: function(response){
-          
+
                 $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Datos enviados satisfactoriamente.</span>');
-              
-            
+
+
          },
          error: function(error){
             $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Ocurrió un problema al momento de enviar.</span>');
          }
         });  $('#_agregar').css("opacity","");
         $("#guardar").removeAttr("disabled");
-      
+
     });
-  
+
 });
 
 // Agregando categoria de post
@@ -139,15 +139,15 @@ $('#formcategoria').on('submit', function(e){
         async:true,
         data: $("#formcategoria").serialize(),
         success: function(response){
-          
+
                $('#_categoria').html(data);
-             
-           
+
+
         },
         error: function(error){
             console.log(error);
         }
-      
+
     });
    	$('#categoria').modal('hide');
 });
@@ -172,7 +172,7 @@ $('#proveedores').on('submit', function(e){
             if(msg==0){
                 $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Datos enviados satisfactoriamente.</span>');
             }else{
-               
+
                 $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Ocurrió un problema al momento de enviar.</span>');
             }
             $('#proveedores').css("opacity","");
@@ -202,7 +202,7 @@ $('#addusuario').on('submit', function(e){
             if(msg==0){
                 $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Datos enviados satisfactoriamente.</span>');
             }else{
-               
+
                 $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Ocurrió un problema al momento de enviar.</span>');
             }
             $('#addusuario').css("opacity","");
@@ -219,15 +219,15 @@ $('#categoria_usuario').on('submit', function(e){
         async:true,
         data: $("#categoria_usuario").serialize(),
         success: function(response){
-          
+
                $('#_categoria').html(response);
-             
-           
+
+
         },
         error: function(error){
             console.log(error);
         }
-      
+
     });
    	$('#modalUser').modal('hide');
 });
@@ -244,7 +244,7 @@ $("#productos").on('submit', function(e){
         cache: false,
         processData:false,
         beforeSend: function(){
-            $('#guardar').attr("disabled","disabled");
+            $('#guardar_producto').attr("disabled","disabled");
             $('#productos').css("opacity",".5");
         },
         success: function(msg){
@@ -252,32 +252,42 @@ $("#productos").on('submit', function(e){
             if(msg==0){
                 $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Datos enviados satisfactoriamente.</span>');
             }else{
-               
+
                 $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Ocurrió un problema al momento de enviar.</span>');
             }
             $('#productos').css("opacity","");
-            $("#guardar").removeAttr("disabled");
+            $("#guardar_producto").removeAttr("disabled");
         }
     });
 });
 
 // aperturando modales
 $(document).ready(function(){
-  
+
     $('#mostrar_ocultar').click(function(){
         $("#modal_productos").modal('show');
-       
+
     });
     $('#cliente').click(function(){
         $('#modal_clientes').modal('show');
     });
-    
+    $('#codigo').click(function(){
+        $('#_productos').modal('show');
+    });
+    $('#descripcion').click(function(){
+        $('#_productos').modal('show');
+    });
+    $('#precio').click(function(){
+        $('#_productos').modal('show');
+    });
+
+
     $('#mostrar').click(function(){
         $('#productos_catalago').slideToggle("slow");
-    
+
     })
-  
-  
+
+
 });
 
 // animacion login
@@ -289,7 +299,7 @@ const container = document.querySelector(".container");
 // clientes para ventas
 // Tabla de clientes
 $(document).ready(function() {
-    
+
     var opcion;
     opcion = 2;
     // Función para cambiar el idioma de la datatable
@@ -332,36 +342,37 @@ $(document).ready(function() {
                     }
                 }
             }
-    
-    
-                
+
+
+
     tablaclientes = $('#clientes_ventas').DataTable({
         "paging": true,
         "lengthChange": true,
         "searching": true,
         "ordering": true,
         "info": true,
+        "scrollX": true,
         "autoWidth": true,
         "language": idioma,
         "lengthMenu": [[5,20,30, -1],[5,20,30,"Mostrar Todo"]],
         dom: 'Bfrt<"col-md-6 inline"i> <"col-md-6 inline"p>',
 
     //AJAX//
-        "ajax":{            
-            "url": "../CRUD/crud.php", 
+        "ajax":{
+            "url": "../CRUD/crud.php",
             "method": 'POST', //usamos el metodo POST
             "data":{opcion:opcion}, //enviamos opcion 1 para que haga un SELECT
             "dataSrc":""
         },
         "columns":[
-        
+
             {"data": "id"},
             {"data": "identidad"},
             {"data": "cliente"},
             {"data": "direccion"},
             {"data": "telefono"},
             {"data": "email"},
-          
+
             {"defaultContent":"<div class='text-center'><div class=''><a class='btn-floating  seleccionar'><i class='fas fa-clipboard-check '></i></a></div></div>"}
          ],
         "columnDefs": [
@@ -387,24 +398,24 @@ $(document).ready(function() {
                 tag: null
               }
             },
-            
+
             buttons: [
-                    
-                      
-                     
+
+
+
                       {
                           extend:    'pageLength',
                           titleAttr: 'Registros a mostrar',
                           className: 'selectTable'
                       }
                   ]
-            
-            
+
+
           }
-    }); 
-    $(document).on("click",".seleccionar",function(){     
-        fila = $(this).closest("tr");	
-        id_cliente = parseInt(fila.find('td:eq(1)').text()); //capturo el ID	
+    });
+    $(document).on("click",".seleccionar",function(){
+        fila = $(this).closest("tr");
+        id_cliente = parseInt(fila.find('td:eq(1)').text()); //capturo el ID
         nombre = fila.find('td:eq(2)').text();
         email = fila.find('td:eq(5)').text();
         telefono = fila.find('td:eq(4)').text();
@@ -412,13 +423,216 @@ $(document).ready(function() {
         $("#cliente").val(nombre);
         $("#telefono").val(telefono);
         $("#email").val(email);
-       
-        $('#modal_clientes').modal('hide');	    
-    }); 
 
-   
-}); 
+        $('#modal_clientes').modal('hide');
+    });
 
+
+});
+
+// Tabla de productos
+
+$(document).ready(function() {
+
+    var opcion;
+
+    // Función para cambiar el idioma de la datatable
+    var idioma=
+
+            {
+                "sProcessing":     "Procesando...",
+                "sLengthMenu":     "Mostrar _MENU_ registros",
+                "sZeroRecords":    "No se encontraron resultados",
+                "sEmptyTable":     "Ningun dato disponible en esta tabla",
+                "sInfo":           "_START_ - _END_  De _TOTAL_ Registros",
+                "sInfoEmpty":      "0 de 0 de un total de 0 registros",
+                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix":    "",
+                "sSearch":         "Buscar:",
+                "sUrl":            "",
+                "sInfoThousands":  ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst":    "<i class='fas fa-fast-forward'></i>",
+                    "sLast":     "<i class='fas fa-fast-backward'></i>",
+                    "sNext":     "<i class='fas fa-forward'></i>",
+                    "sPrevious": "<i class='fas fa-backward'></i>"
+                },
+                "oAria": {
+                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                },
+                "buttons": {
+                    "copyTitle": 'Informacion copiada',
+                    "copyKeys": 'Use your keyboard or menu to select the copy command',
+                    "copySuccess": {
+                        "_": '%d filas copiadas al portapapeles',
+                        "1": '1 fila copiada al portapapeles'
+                    },
+
+                    "pageLength": {
+                    "_": "Mostrar %d filas",
+                    "-1": "Mostrar Todo"
+                    }
+                }
+            }
+
+
+
+       tablaProductos = $('#tabla_productos').DataTable({
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "scrollX": true,
+        "info": true,
+        "autoWidth": true,
+        "language": idioma,
+        "lengthMenu": [[2,20,30, -1],[2,20,30,"Mostrar Todo"]],
+        dom: 'Bfrt<"col-md-6 inline"i> <"col-md-6 inline"p>',
+
+        buttons: {
+            dom: {
+              container:{
+                tag:'div',
+                className:'flexcontent'
+              },
+              buttonLiner: {
+                tag: null
+              }
+            },
+
+            buttons: [
+
+                      {
+                          extend:    'pageLength',
+                          titleAttr: 'Registros a mostrar',
+                          className: 'selectTable'
+                      }
+                  ]
+
+
+          }
+    });
+    $(document).on("click",".btnSeleccionar",function(){
+
+
+        fila = $(this).closest("tr");
+        codigo=parseInt(fila.find('td:eq(0)').text());
+
+        nombre = fila.find('td:eq(2)').text();
+        precio = fila.find('td:eq(3)').text();
+        $("#codigo").val(codigo);
+        $("#descripcion").val(nombre);
+        $("#precio").val(precio);
+        $('#_productos').modal('hide');
+    });
+
+
+});
+
+
+
+// Categoria de productos
+
+$(document).ready(function() {
+
+    var opcion;
+
+    // Función para cambiar el idioma de la datatable
+    var idioma=
+
+            {
+                "sProcessing":     "Procesando...",
+                "sLengthMenu":     "Mostrar _MENU_ registros",
+                "sZeroRecords":    "No se encontraron resultados",
+                "sEmptyTable":     "Ningun dato disponible en esta tabla",
+                "sInfo":           "_START_ - _END_  De _TOTAL_ Registros",
+                "sInfoEmpty":      "0 de 0 de un total de 0 registros",
+                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix":    "",
+                "sSearch":         "Buscar:",
+                "sUrl":            "",
+                "sInfoThousands":  ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst":    "<i class='fas fa-fast-forward'></i>",
+                    "sLast":     "<i class='fas fa-fast-backward'></i>",
+                    "sNext":     "<i class='fas fa-forward'></i>",
+                    "sPrevious": "<i class='fas fa-backward'></i>"
+                },
+                "oAria": {
+                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                },
+                "buttons": {
+                    "copyTitle": 'Informacion copiada',
+                    "copyKeys": 'Use your keyboard or menu to select the copy command',
+                    "copySuccess": {
+                        "_": '%d filas copiadas al portapapeles',
+                        "1": '1 fila copiada al portapapeles'
+                    },
+
+                    "pageLength": {
+                    "_": "Mostrar %d filas",
+                    "-1": "Mostrar Todo"
+                    }
+                }
+            }
+
+
+
+       tablaProductos = $('#categoria_producto').DataTable({
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "scrollX": true,
+        "info": true,
+        "autoWidth": true,
+        "language": idioma,
+        "lengthMenu": [[2,20,30, -1],[2,20,30,"Mostrar Todo"]],
+        dom: 'Bfrt<"col-md-6 inline"i> <"col-md-6 inline"p>',
+
+        buttons: {
+            dom: {
+              container:{
+                tag:'div',
+                className:'flexcontent'
+              },
+              buttonLiner: {
+                tag: null
+              }
+            },
+
+            buttons: [
+
+                      {
+                          extend:    'pageLength',
+                          titleAttr: 'Registros a mostrar',
+                          className: 'selectTable'
+                      }
+                  ]
+
+
+          }
+    });
+    // $(document).on("click",".btnSeleccionar",function(){
+
+
+    //     fila = $(this).closest("tr");
+    //     codigo=parseInt(fila.find('td:eq(0)').text());
+
+    //     nombre = fila.find('td:eq(2)').text();
+    //     precio = fila.find('td:eq(3)').text();
+    //     $("#codigo").val(codigo);
+    //     $("#descripcion").val(nombre);
+    //     $("#precio").val(precio);
+    //     $('#_productos').modal('hide');
+    // });
+
+
+});
 
 
 // editor de texto
@@ -436,7 +650,66 @@ $(document).ready(function(){
 			success:function(data){
 				$(".items").html(data).fadeIn('slow');
 		}
-		});
+        });
+        
+
+        $( "#guardar_item" ).submit(function( event ) {
+            parametros = $(this).serialize();
+            $.ajax({
+                type: "POST",
+                url:'../CRUD/productos.php',
+                data: parametros,
+                beforeSend: function(objeto){
+                    $('.items').html('Cargando...');
+                },
+                success:function(data){
+                    $(".items").html(data).fadeIn('slow');
+
+                }
+            })
+
+        event.preventDefault();
+        })
+        $("#guardar").click(function(){
+            
+             var cliente = $("#idCliente").val();
+             
+     
+     
+             if (cliente>0)
+            {
+               VentanaCentrada('../pdf/documentos/orden.php?cliente='+cliente);
+            } else {
+                alert("Selecciona el cliente");
+                return false;
+            }
+     
+        });
 })
+
+
+function eliminar_item(id){
+    $.ajax({
+        type: "GET",
+        url: "../CRUD/productos.php",
+        data: "action=ajax&id="+id,
+         beforeSend: function(objeto){
+             $('.items').html('Cargando...');
+          },
+        success: function(data){
+            $(".items").html(data).fadeIn('slow');
+        }
+    });
+    }
+
+    function VentanaCentrada(theURL,winName,features, myWidth, myHeight, isCenter) { //v3.0
+        if(window.screen)if(isCenter)if(isCenter=="true"){
+          var myLeft = (screen.width-myWidth)/2;
+          var myTop = (screen.height-myHeight)/2;
+          features+=(features!='')?',':'';
+          features+=',left='+myLeft+',top='+myTop;
+        }
+        window.open(theURL,winName,features+((features!='')?',':'')+'width='+myWidth+',height='+myHeight);
+      }
 
 // USUARIOS
